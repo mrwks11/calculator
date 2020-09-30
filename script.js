9f2f59eb386faff895c2ae402d66526838a7b32a
mrwks11 <w.k.sinke@gmail.com>
2020-09-30 11:31:41 +0200

    Fixed keyboard keys + extra styling.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Declaration of variables and initial values.
 let userInput = '';
 let equation = '';
 let operator = '';
@@ -8,104 +7,152 @@ let result;
 
 const display = document.getElementById('display');
 
-// When user enters a number.
-const numberButtons = document.querySelectorAll('.number-button');
+// KEYBOARD SUPPORT
+window.addEventListener('keydown', function(e) {
+    const key = document.querySelector(`button[value='${e.key}']`);
+    key.click();
+});
+
+// NUMBERS
+const numberButtons = document.querySelectorAll('.number');
 for (let i = 0 ; i < numberButtons.length; i++) {
     numberButtons[i].addEventListener('click', () => {
-    userInput += numberButtons[i].innerText;
-    userInput = parseInt(userInput);
-    display.textContent = `${equation} ${userInput}`;
-    })
+        userInput += numberButtons[i].value;
+        display.textContent = `${equation}${userInput}`;
+    });
 };
 
-// When user selects to 'add'.
-const addButton = document.querySelector('.add-button');
+// ADD
+const addButton = document.querySelector('.add');
 addButton.addEventListener('click', () => {
     checkInput();
     operator = 'add';
     num1 = userInput;
-    equation = `${num1} + `;
+    equation = `${num1}+`;
     display.textContent = equation;
     userInput = '';
 });
 
-// When user selects to 'subtract'.
-const subtractButton = document.querySelector('.subtract-button');
+// SUBTRACT
+const subtractButton = document.querySelector('.subtract');
 subtractButton.addEventListener('click', () => {
     checkInput();
     operator = 'subtract';
     num1 = userInput;
-    equation = `${num1} - `;
+    equation = `${num1}-`;
     display.textContent = equation;
     userInput = '';
 });
 
-// When user selects to 'multiply'.
-const multiplyButton = document.querySelector('.multiply-button');
+// MULTIPLY
+const multiplyButton = document.querySelector('.multiply');
 multiplyButton.addEventListener('click', () => {
     checkInput();
     operator = 'multiply';
     num1 = userInput;
-    equation = `${num1} * `;
+    equation = `${num1}*`;
     display.textContent = equation;
     userInput = '';
 });
 
-// When user selects to 'divide'.
-const divideButton = document.querySelector('.divide-button');
+// DIVIDE
+const divideButton = document.querySelector('.divide');
 divideButton.addEventListener('click', () => {
     checkInput();
     operator = 'divide';
     num1 = userInput;
-    equation = `${num1} / `;
+    equation = `${num1}/`;
     display.textContent = equation;
     userInput = '';
 });
 
-// Display result to user.
-const resultButton = document.querySelector('.result-button');
+// MODULUS
+const modulusButton = document.querySelector('.modulus');
+modulusButton.addEventListener('click', () => {
+    checkInput();
+    operator = 'modulus';
+    num1 = userInput;
+    equation = `${num1}%`;
+    display.textContent = equation;
+    userInput = '';
+});
+
+// RESULT
+const resultButton = document.querySelector('.result');
 resultButton.addEventListener('click', () => {
     num2 = userInput;
     operate(operator, num1, num2);
     display.textContent = result;
-    
 });
 
-// Clear everything and start again.
-const clearButton = document.querySelector('.clear-button');
+// CLEAR
+const clearButton = document.querySelector('.clear');
 clearButton.addEventListener('click', () => {
-equation = '';
-userInput = '';
-num1 = undefined;
-num2 = undefined;
-operator = undefined;
-result = undefined;
-display.textContent = userInput;
+    equation = '';
+    userInput = '';
+    num1 = undefined;
+    num2 = undefined;
+    operator = undefined;
+    result = undefined;
+    display.textContent = userInput;
 });
 
-// Math functions.
+// DECIMAL
+/*
+const decimalButton = document.querySelector('.decimal');
+decimalButton.addEventListener('click', () => {
+    userInput += decimalButton.innerText;
+    display.textContent = `${equation} ${userInput}`;
+});
+*/
+
+// BACKSPACE
+const backspaceButton = document.querySelector('.backspace');
+backspaceButton.addEventListener('click', () => {
+    userInput = userInput.slice(0, -1);
+    display.textContent = userInput;
+});
+
+// REVERSE
+/*
+const reverseButton = document.querySelector('.reverse');
+reverseButton.addEventListener('click', () => {
+    checkInput();
+    userInput = userInput * -1;
+    num1 = userInput;
+    display.textContent = userInput;
+});
+*/
+
+// MATH FUNCTIONS
 function add(num1, num2) {
-    result = Math.round((num1 + num2) * 100000) / 100000;
+    result = Math.round((num1 + num2) * 1000) / 1000;
 }
 
 function subtract(num1, num2) {
-    result = Math.round((num1 - num2) * 100000) / 100000;
+    result = Math.round((num1 - num2) * 1000) / 1000;
 }
 
 function multiply(num1, num2) {
-    result = Math.round((num1 * num2) * 100000) / 100000;
+    result = Math.round((num1 * num2) * 1000) / 1000;
 }
 
 function divide(num1, num2) {
     if (num2 === 0) {
         result = 'ERROR!';
     } else {
-        result = Math.round((num1 / num2) * 100000) / 100000;
+        result = Math.round((num1 / num2) * 1000) / 1000;
     }
 }
 
-// Checks selected operator and inputs user's equation into math functions.
+function modulus(num1, num2) {
+    result = num1 % num2;
+}
+
+// INPUT USER VALUES INTO MATH FUNCTIONS
 function operate(operator, num1, num2) {
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
     if (operator === 'add') {
         add(num1, num2);
         return result;
@@ -118,10 +165,13 @@ function operate(operator, num1, num2) {
     } else if (operator === 'divide') {
         divide(num1, num2);
         return result;
-    } 
+    } else if (operator === 'modulus') {
+        modulus(num1, num2);
+        return result;
+    }
 }
 
-// When selecting an operator, check previous input (num1) first.
+// CHECK PREVIOUS INPUT
 function checkInput() {
     if (num1 === undefined) {
         num1 = userInput;
